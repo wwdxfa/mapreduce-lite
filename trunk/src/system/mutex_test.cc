65c22455efd89fb579c398d01073cb24a93d01fb
Yi.Wang.2005 <Yi.Wang.2005@51fdd208-ff8f-75da-5072-91cbf86519b3>
2011-06-25 03:18:33 +0000

    Make Mutex portable to BSD, Darwin, Linux and Cygwin

--- trunk/src/system/mutex_test.cc
+++ trunk/src/system/mutex_test.cc
@@ -26,32 +26,23 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 TEST(Mutex, Lock) {
   Mutex mutex;
-  ASSERT_FALSE(mutex.IsLocked());
   mutex.Lock();
-  ASSERT_TRUE(mutex.IsLocked());
   mutex.Unlock();
-  ASSERT_FALSE(mutex.IsLocked());
 }
 
 TEST(Mutex, Locker) {
   Mutex mutex;
   {
-    ASSERT_FALSE(mutex.IsLocked());
     MutexLocker locker(&mutex);
-    ASSERT_TRUE(mutex.IsLocked());
   }
-  ASSERT_FALSE(mutex.IsLocked());
 }
 
 TEST(Mutex, LockerWithException) {
   Mutex mutex;
   try {
-    ASSERT_FALSE(mutex.IsLocked());
     MutexLocker locker(&mutex);
-    ASSERT_TRUE(mutex.IsLocked()) << "after locked constructed";
     throw 0;
   } catch(...) {
     // ignore
   }
-  ASSERT_FALSE(mutex.IsLocked()) << "after exception thrown";
 }
